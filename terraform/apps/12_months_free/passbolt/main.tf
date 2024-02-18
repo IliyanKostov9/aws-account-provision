b70f77394445e9a09ad1670fd9456f740819eb4b
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-02-18 21:40:25 +0200

    Add passbolt module and global variables

--- terraform/apps/12_months_free/passbolt/main.tf
+++ terraform/apps/12_months_free/passbolt/main.tf
@@ -1,6 +1,5 @@
 module "ec2_instance_passbolt" {
-  source = "../../../modules/aws/ec2"
-
+  source            = "../../../modules/aws/ec2"
   ec2_name          = var.ec2_name
   ec2_ami           = var.ec2_ami
   ec2_instance_type = var.ec2_instance_type
